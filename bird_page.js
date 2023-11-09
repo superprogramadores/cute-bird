6d77781df188a8678410b838f765f5a7dfbf2604
superprogramadores <148497927+superprogramadores@users.noreply.github.com>
2023-11-09 14:50:30 -0300

    Add files via upload

---
+++ bird_page.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD999Sg7egW0rsT4iXqRooSp5UuA0gqjIo",
+    authDomain: "bird-3997d.firebaseapp.com",
+    databaseURL: "https://bird-3997d-default-rtdb.firebaseio.com",
+    projectId: "bird-3997d",
+    storageBucket: "bird-3997d.appspot.com",
+    messagingSenderId: "84168879833",
+    appId: "1:84168879833:web:46b91f6c14e090ddccd8c8"
+  };
