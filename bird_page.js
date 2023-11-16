3988264d391dc5f33dce0f20e358378d2d467252
superprogramadores <148497927+superprogramadores@users.noreply.github.com>
2023-11-16 00:56:55 -0300

    Update bird_page.js

--- bird_page.js
+++ bird_page.js
@@ -37,14 +37,43 @@ function getData()
       {
         firebase_message_id = childKey;
         message_data = childData;
+        console.log(firebase_message_id);
+        console.log(message_data);
 //Inicie a programar aqui
 
 
           
 //Programe até aqui
+        name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+        message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+        like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+ like+" onclick='updateLike(this.id)'>";
+        span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Curtidas: "+ like +"</span></button><hr>";
+        row = name_with_tag + message_with_tag +like_button + span_with_tag;       
+        document.getElementById("output").innerHTML += row;
       } 
     });  
   }); 
 }
 
 getData();
+
+function updateLike(message_id)
+{
+  console.log("clicou no botão curtir - " + message_id);
+  //Inicie a programar aqui
+
+
+  //Programe até aqui
+  console.log(updated_likes);
+
+  firebase.database().ref(room_name).child(message_id).update({
+	like : updated_likes  
+  });
+}
+
+function logout() 
+{
+   localStorage.removeItem("user_name");
+   localStorage.removeItem("room_name");
+   window.location.replace("index.html");
+}
