02aa54de3ce1fe8edee4c3a1183e3a148ab042f7
superprogramadores <148497927+superprogramadores@users.noreply.github.com>
2023-11-16 14:20:24 -0300

    Update bird_page.js

--- bird_page.js
+++ bird_page.js
@@ -17,14 +17,7 @@ room_name = localStorage.getItem("room_name");
 
 function send()
 {
-  msg = document.getElementById("msg").value;
-  firebase.database().ref(room_name).push({
-    name:user_name,
-    message:msg,
-    like:0
-   });
-
-  document.getElementById("msg").value = "";
+ 
 }
 
 function getData() 
@@ -73,7 +66,5 @@ function updateLike(message_id)
 
 function logout() 
 {
-   localStorage.removeItem("user_name");
-   localStorage.removeItem("room_name");
-   window.location.replace("index.html");
+  
 }
