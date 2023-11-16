7a5f8aead1a5f528912c016c4bd2ae551e21637f
superprogramadores <148497927+superprogramadores@users.noreply.github.com>
2023-11-16 00:46:48 -0300

    Update bird_page.js

--- bird_page.js
+++ bird_page.js
@@ -1,3 +1,4 @@
+//LINKS DO FIREBASE
 const firebaseConfig = {
     apiKey: "AIzaSyD999Sg7egW0rsT4iXqRooSp5UuA0gqjIo",
     authDomain: "bird-3997d.firebaseapp.com",
@@ -7,3 +8,43 @@ const firebaseConfig = {
     messagingSenderId: "84168879833",
     appId: "1:84168879833:web:46b91f6c14e090ddccd8c8"
   };
+
+//inicializar o Firebase
+firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+
+function send()
+{
+  msg = document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
+    name:user_name,
+    message:msg,
+    like:0
+   });
+
+  document.getElementById("msg").value = "";
+}
+
+function getData() 
+{ 
+  firebase.database().ref("/"+room_name).on('value', function(snapshot) { 
+    document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { 
+      childKey  = childSnapshot.key; 
+      childData = childSnapshot.val(); 
+      if(childKey != "purpose") 
+      {
+        firebase_message_id = childKey;
+        message_data = childData;
+//Inicie a programar aqui
+
+
+          
+//Programe até aqui
+      } 
+    });  
+  }); 
+}
+
+getData();
