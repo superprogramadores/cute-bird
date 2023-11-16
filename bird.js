e01d3d5a920db25329c5431a275c42f94461450e
superprogramadores <148497927+superprogramadores@users.noreply.github.com>
2023-11-16 00:42:04 -0300

    Update bird.js

--- bird.js
+++ bird.js
@@ -6,5 +6,5 @@ user_name = document.getElementById("user_name").value;
 // Armazene o valor da variável user_name no armazenamento local com a chave user_name
 localStorage.setItem("user_name", user_name);
 // Redirecione os usuários para kwitter_room.html.
-window.location = "kwitter_room.html";
+window.location = "bird_sala.html";
 }
